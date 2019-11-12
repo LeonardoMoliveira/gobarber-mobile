e7db8e7f26ac934268a926f1866d50ef9775c614
leonardoMoliveira <leonardo.m.oliveira98@gmail.com>
2019-11-12 14:45:13 -0300

    finished essentials

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useMemo } from 'react';
+import { parseISO, formatRelative } from 'date-fns';
+import pt from 'date-fns/locale/pt';
 import { TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import { Container, Left, Avatar, Info, Name, Time } from './styles';
 
-export default function Appointment() {
+export default function Appointment({ data }) {
+  /* const dateParsed = useMemo(() => {
+    return formatRelative(parseISO(data.date), new Date(), {
+      locale: pt,
+      addSuffix: true,
+    });
+  }, [data.date]); */
+
   return (
     <Container>
       <Left>
@@ -13,8 +22,8 @@ export default function Appointment() {
         />
 
         <Info>
-          <Name>Leonardo Moraes</Name>
-          <Time>em 3 horas</Time>
+          <Name>{data.name}</Name>
+          <Time>dateParsed</Time>
         </Info>
       </Left>
 
--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,23 +1,67 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+import { withNavigationFocus } from 'react-navigation';
+import PropTypes from 'prop-types';
+// import api from '~/services/api';
 
 import Background from '~/components/Background';
 import Appointment from '~/components/Appointment';
 
 import { Container, Title, List } from './styles';
 
-const data = [1, 2, 3, 4, 5];
+function Dashboard({ isFocused }) {
+  const [appointments, setAppointments] = useState([]);
 
-export default function Dashboard() {
+  async function loadAppointments() {
+    /* const response = await api.get('appointments');
+
+    setAppointments(response.data); */
+
+    setAppointments([
+      {
+        id: 1,
+        date: new Date(),
+        user_id: 1,
+        provider_id: 1,
+        canceled_at: new Date(),
+        updated_at: new Date(),
+        name: 'Leonardo',
+      },
+      {
+        id: 2,
+        date: new Date(),
+        user_id: 1,
+        provider_id: 1,
+        canceled_at: new Date(),
+        updated_at: new Date(),
+        name: 'Leonardo',
+      },
+      {
+        id: 3,
+        date: new Date(),
+        user_id: 1,
+        provider_id: 1,
+        canceled_at: new Date(),
+        updated_at: new Date(),
+        name: 'Leonardo',
+      },
+    ]);
+  }
+
+  useEffect(() => {
+    if (isFocused) {
+      loadAppointments();
+    }
+  }, [isFocused]);
+
   return (
     <Background>
       <Container>
         <Title>Agendamentos</Title>
 
         <List
-          data={data}
-          keyExtractor={item => String(item)}
+          data={appointments}
+          keyExtractor={item => String(item.id)}
           renderItem={({ item }) => <Appointment data={item} />}
         />
       </Container>
@@ -27,7 +71,14 @@ export default function Dashboard() {
 
 Dashboard.navigationOptions = {
   tabBarLabel: 'Agendamentos',
+  // eslint-disable-next-line react/prop-types
   tabBarIcon: ({ tintColor }) => (
     <Icon name="event" size={20} color={tintColor} />
   ),
 };
+
+Dashboard.propTypes = {
+  isFocused: PropTypes.bool.isRequired,
+};
+
+export default withNavigationFocus(Dashboard);
